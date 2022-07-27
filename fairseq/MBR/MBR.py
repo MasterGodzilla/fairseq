1873b35ae49b41dd903c6c9896db805d3c1836ac
Hanchi Sun <has423@lehigh.edu>
2022-07-27 17:46:07 -0400

    optimize MBR

--- fairseq/MBR/MBR.py
+++ fairseq/MBR/MBR.py
@@ -2,6 +2,7 @@ from pickle import TRUE
 from typing import List
 import sacrebleu
 import torch
+from time import time
 
 
 def min_bayes_risk1(hypos_i, sample_size, reference_func="BLEU"):
@@ -35,59 +36,17 @@ def min_bayes_risk1(hypos_i, sample_size, reference_func="BLEU"):
     return:
         hypos with score as the expected utility
     """
-    e_utility = [0.0 for _ in range(sample_size)]
+    #Optimization: since bleu(j,j) is 100 and bleu (j,k) = bleu (k,j), we save half calculations by symmetry
+    bleu_time = 0
     for j in range(sample_size):
         for k in range(sample_size):
             if reference_func == "BLEU":
+                tic = time()
                 utility = sacrebleu.corpus_bleu([hypos_i[j]["detok_str"]], [[hypos_i[k]["detok_str"]]]).score
-            e_utility[j] += (utility* torch.exp(hypos_i[k]["score"]).item())
-        
-    for j in range(sample_size):
-        hypos_i[j]["expected_utility"] = e_utility[j]
+                bleu_time += time()-tic
+            hypos_i[j]["expected_utility"] += (utility * torch.exp(hypos_i[k]["score"]).item())
     
     #sort expected utility in descending order
     hypos_i.sort(key = lambda hypo: hypo.get("expected_utility"),reverse=True)
-
+    print ("bleu time:", bleu_time)
     return hypos_i
-
-def min_bayes_risk(hypos, sample_size, utility="BLEU"):
-    """
-    This function changes the score of the input hypos to the expected utility function, 
-    so the outside ranking function can arrange it accordingly. See
-
-        Sampling-Based Minimum Bayes Risk Decoding for Neural Machine Translation
-        https://arxiv.org/pdf/2108.04718.pdf
-    
-    Args:
-        hypos (list(size batch_size) of list (size beam_size) of Dict with attributes
-            "tokens": torch.Tensor with len sentence_length and dtype int (token id)
-            "score": torch.Tensor of size 1 float
-            "attention": empty torch.Tensor
-            "alignment":
-            "positional_scores": torch.Tensor() len sentence_length dtype float)
-        
-        sample_size (int): the size of sample/cfg.beam_size
-        utility (str, optional): choices of 
-            "BLEU"
-            "BEER"
-            "METEOR"
-            "ChrF"
-    
-    return:
-        hypos with score as the expected utility
-    """
-    #print (type(hypos))
-    #print ("hypos.size()",hypos.size())
-    #print ("hypos[0]", hypos[0])
-
-    for i in range(len(hypos)):
-        e_utility = [0.0 for _ in range(sample_size)]
-        for j in range(sample_size):
-            for k in range(sample_size):
-                e_utility[j] += (sacrebleu.corpus_bleu(hypos[i][j]["tokens"], [hypos[i][k]["tokens"]], tokenize = "none") 
-                * torch.exp(hypos[i][k]["score"]))
-            
-        for j in range(sample_size):
-            hypos[i][j]["score"] = e_utility[j]
-    
-    return hypos
