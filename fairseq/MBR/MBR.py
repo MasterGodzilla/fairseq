e79b62dfa15493b266c09da531dfa61850971082
Hanchi Sun <has423@lehigh.edu>
2022-07-26 21:14:39 -0400

    access bleu_score.score

--- fairseq/MBR/MBR.py
+++ fairseq/MBR/MBR.py
@@ -38,9 +38,7 @@ def min_bayes_risk1(hypos_i, sample_size, utility="BLEU"):
     for j in range(sample_size):
         for k in range(sample_size):
             bleu_score = sacrebleu.corpus_bleu(hypos_i[j]["detok_str"], [hypos_i[k]["detok_str"]], tokenize = "none")
-            print (bleu_score)
-            print (bleu_score[0])
-            e_utility[j] += (bleu_score[0]
+            e_utility[j] += (bleu_score.score
             * torch.exp(hypos_i[k]["score"]).item())
         
     for j in range(sample_size):
